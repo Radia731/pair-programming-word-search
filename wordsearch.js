4602530afcb176206f8229c688fc0c2a7212c171
Radia <126217826+Radia731@users.noreply.github.com>
2023-05-20 13:18:22 -0400

    Update wordsearch.js
    first commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,52 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalTranspose = transpose(letters); /
+    const verticalJoin = verticalTranspose.map(ls => ls.join(''))
+
+  
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      
+
+        if (word.length === verticalJoin.length) {
+           
+            if (l.includes(word)){
+                // If found return true
+                return true;
+            } 
+        }
     }
+   
+    for (l of verticalJoin) {
+  
+
+        if (word.length === horizontalJoin.length) {
+ 
+            if (l.includes(word)){
+                // If found return true
+                return true;
+            }  
+        }
+    }
+
+  
+    return false;
+}
+
+const transpose = function(matrix) {
+    
+    const vertical = matrix.length;
+    const horizontal = matrix[0].length;
+    const verticalArray = [];
+
+    for (let h = 0; h < horizontal; h++) {
+        verticalArray[h] = [];
+        for (let v = 0; v < vertical; v++) {
+           
+            verticalArray[h][v] = matrix[v][h];
+        }
+    }
+    return verticalArray;
 }
 
 module.exports = wordSearch
+
